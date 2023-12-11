0e640600932e2079515888fd693b1078757dadef
Usama <usamamian7166@gmail.com>
2023-12-11 22:10:43 +0500

    After completing the UI of Book Store management System and before adding the database

---
+++ src/bookstore_management_system/BookStore_Management_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bookstore_management_system;
+
+/**
+ *
+ * @author usama
+ */
+public class BookStore_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
